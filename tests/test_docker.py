bc05bdd1fc899c501485361963a034cbfacd5fe4
Thibault Derousseaux <tibdex@gmail.com>
2021-10-01 18:50:17 +0200

    Enable BuildKit only when building the image

--- tests/test_docker.py
+++ tests/test_docker.py
@@ -1,5 +1,5 @@
 from subprocess import check_call
-import time
+from time import sleep
 from http import HTTPStatus
 
 import docker
@@ -10,8 +10,15 @@ SESSION_PORT = 9090
 
 
 def test_docker_container():
-    check_call(["docker", "build", "--tag", IMAGE_TAG, "."])
+    # BuildKit is not supported by Docker's Python SDK.
+    # See https://github.com/docker/docker-py/issues/2230.
+    check_call(
+        ["docker", "build", "--tag", IMAGE_TAG, "."],
+        env={"DOCKER_BUILDKIT": 1},
+    )
+
     client = docker.from_env()
+
     try:
         container = client.containers.run(
             image=IMAGE_TAG,
@@ -19,7 +26,7 @@ def test_docker_container():
             detach=True,
         )
         while "Session running" not in str(container.logs()):
-            time.sleep(1)
+            sleep(1)
         response = requests.get(f"http://localhost:{SESSION_PORT}/versions/rest")
         assert response.status_code == HTTPStatus.OK
     finally:
