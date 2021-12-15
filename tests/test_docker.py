03944a535f373c1db4b45dca555a01bb30d9a9ce
Thibault Derousseaux <tibdex@gmail.com>
2021-12-15 15:43:33 +0100

    fixup! Update deps

--- tests/test_docker.py
+++ tests/test_docker.py
@@ -1,23 +1,29 @@
+import os
 from http import HTTPStatus
 from subprocess import check_call
 from time import sleep
 
 import docker
+import pytest
 import requests
 
 IMAGE_TAG = "atoti-project-template"
 SESSION_PORT = 9090
 
 
-def test_docker_container() -> None:
+@pytest.fixture(name="docker_image_built", scope="module")
+def build_docker_image() -> None:
     # BuildKit is not supported by Docker's Python SDK.
     # See https://github.com/docker/docker-py/issues/2230.
     check_call(
         ["docker", "build", "--tag", IMAGE_TAG, "."],
         env={"DOCKER_BUILDKIT": "1"},
-        # shell=True,
+        shell=os.environ.get("CI", "false").lower() != "true",
     )
 
+
+@pytest.mark.usefixtures("docker_image_built")
+def test_docker_container() -> None:
     client = docker.from_env()
 
     container = client.containers.run(
--- tests/test_session.py
+++ tests/test_session.py
@@ -1,11 +1,20 @@
+from typing import Generator
+
+import atoti as tt
 import pandas as pd
+import pytest
 from pandas._testing import assert_frame_equal
 
 from app import start_session
 
 
-def test_price_sum() -> None:
-    session = start_session()
+@pytest.fixture(name="session")
+def session_fixture() -> Generator[tt.Session, None, None]:
+    with start_session() as session:
+        yield session
+
+
+def test_price_sum(session: tt.Session) -> None:
     cube = session.cubes["Products"]
     actual_result = cube.query(cube.measures["Price.SUM"])
     expected_result = pd.DataFrame(
