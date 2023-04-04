d2d9f24dc9473df6354c3fa38565fcf5e2947581
Thibault Derousseaux <tibdex@gmail.com>
2023-04-04 14:50:51 -0400

    Upgrade dependencies

--- app/config.py
+++ app/config.py
@@ -1,8 +1,11 @@
+# ruff: noqa: UP007
+# Pydantic evaluates type annotations at runtime which does not support `|`.
+
 from __future__ import annotations
 
 from datetime import timedelta
 from pathlib import Path
-from typing import Annotated, Any
+from typing import Annotated, Optional, Union
 
 from pydantic import (
     BaseSettings,
@@ -24,19 +27,19 @@ class Config(BaseSettings):
     See https://pydantic-docs.helpmanual.io/usage/settings/.
     """
 
-    data_refresh_period: timedelta | None = timedelta(minutes=1)
+    data_refresh_period: Optional[timedelta] = timedelta(minutes=1)
 
     # The $PORT environment variable is used by most PaaS to indicate the port the app server should bind to.
     port: int = 9090
 
     requests_timeout: timedelta = timedelta(seconds=30)
 
-    reverse_geocoding_path: HttpUrl | FilePath = parse_obj_as(
+    reverse_geocoding_path: Union[HttpUrl, FilePath] = parse_obj_as(
         HttpUrl, "https://api-adresse.data.gouv.fr/reverse/csv/"
     )
 
     user_content_storage: Annotated[
-        PostgresDsn | Path | None,
+        Optional[Union[PostgresDsn, Path]],
         Field(
             # $DATABASE_URL is used by some PaaS such to designate the URL of the app's primary database.
             # For instance: https://devcenter.heroku.com/articles/heroku-postgresql#designating-a-primary-database.
@@ -44,14 +47,14 @@ class Config(BaseSettings):
         ),
     ] = Path("content")
 
-    velib_data_base_path: HttpUrl | DirectoryPath = parse_obj_as(
+    velib_data_base_path: Union[HttpUrl, DirectoryPath] = parse_obj_as(
         HttpUrl,
         "https://velib-metropole-opendata.smoove.pro/opendata/Velib_Metropole",
     )
 
     @validator("user_content_storage")
     @classmethod
-    def normalize_postgresql_dsn(cls, value: PostgresDsn | Any) -> object:
+    def normalize_postgresql_dsn(cls, value: PostgresDsn | object) -> object:
         return (
             normalize_postgres_dsn_for_atoti_sql(value)
             if isinstance(value, PostgresDsn)
--- tests/docker/conftest.py
+++ tests/docker/conftest.py
@@ -11,7 +11,7 @@ import docker
 import pytest
 from docker.models.containers import Container
 
-from ._docker_container import docker_container
+from ._docker_container import docker_container as _docker_container
 from ._run_command import run_command
 from ._timeout import Timeout
 
@@ -31,7 +31,7 @@ def poetry_executable_path_fixture() -> str:
 
 
 @pytest.fixture(name="docker_image_name", scope="session")
-def docker_image_name_fixture(
+def docker_image_tag_fixture(
     docker_executable_path: str, poetry_executable_path: str, project_name: str
 ) -> Generator[str, None, None]:
     tag = f"{project_name}:{uuid4()}"
@@ -60,7 +60,7 @@ def docker_container_fixture(
 ) -> Generator[Container, None, None]:
     timeout = Timeout(timedelta(minutes=1))
 
-    with docker_container(docker_image_name, client=docker_client) as container:
+    with _docker_container(docker_image_name, client=docker_client) as container:
         logs = container.logs(stream=True)
 
         while "Session listening on port" not in next(logs).decode():
