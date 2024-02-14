7c360f9e35d5e7d34423bd339df4f8f9d6ce1cf5
Thibault Derousseaux <tibdex@gmail.com>
2024-02-14 09:37:58 -0500

    fixup! Update to Atoti Python API 0.8.9

--- app/__main__.py
+++ app/__main__.py
@@ -4,4 +4,4 @@ from . import Config, start_app
 
 with start_app(config=Config()) as session:
     print(f"Session listening on port {session.port}")  # noqa: T201
-    input("Press Enter to stop the application")
+    session.wait()
