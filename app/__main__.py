99311c3de2326f5c004cb7b652eb4ccd4c54e997
Thibault Derousseaux <tibdex@gmail.com>
2024-12-06 15:28:10 +0100

    fixup! Update to Atoti Python API 0.9.2

--- app/__main__.py
+++ app/__main__.py
@@ -7,7 +7,7 @@ from . import Config, start_app
 async def main() -> None:
     async with start_app(config=Config()) as session:
         port = urlparse(session.url).port or 80
-        print(f"Session listening on port {port}")
+        print(f"Session listening on port {port}")  # noqa: T201
         await asyncio.to_thread(session.wait)
 
 
